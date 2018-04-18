c4da4df8012ab4e8304fdef06680b27ec2151e4b
Estefania Akers <estefaniaakers@hotmail.com>
2018-04-18 13:39:35 -0500

    adding bird and pipes

--- flappyBird.js
+++ flappyBird.js
@@ -11,13 +11,61 @@ var pipeSouth = new Image();
 
 bird.src = "Images/bird.png";
 bg.src = "Images/bg.png";
-fg.src = "images/fg.png";
-pipeNorth.src = "images/pipeNorth.png";
-pipeSouth.src = "images/pipeSouth.png";
+fg.src = "Images/fg.png";
+pipeNorth.src = "Images/pipeNorth.png";
+pipeSouth.src = "Images/pipeSouth.png";
+
+//some variables
+var gap = 85;
+var constant = pipeNorth.height+gap;
+
+var bX = 10;
+var bY = 150
+var gravity = 1.5;
+
+//key pressed
+document.addEventListener('keydown',moveUp);
+function moveUp(){
+    bY -= 25;
+
+}
+
+//pipe coordinates
+var pipe = [];
+
+pipe[0] = {
+    x : cvs.width,
+    y : 0
+ };
 
 // draw images
 
-function draw(){}
+function draw(){
+
+    ctx.drawImage(bg,0,0);
+
+    for(var i=0; i<pipe.length; i++){
+       ctx.drawImage(pipeNorth,pipe[i].x,pipe[i].y);
+       ctx.drawImage(pipeSouth,pipe[i].x,pipe[i].y+constant);
+
+       pipe[i].x--;
+
+       if(pipe[i].x == 125){
+        pipe.push({
+            x : cvs.width,
+            y : Math.floor(Math.random()*pipeNorth.height)-pipeNorth.height
+        });
+
+        }
+    
+    }
+    
+    ctx.drawImage(fg,0,cvs.height - fg.height);
+    ctx.drawImage(bird,bX,bY);
+
+    bY += gravity;
+    requestAnimationFrame(draw);
+}
 
 draw();
 
