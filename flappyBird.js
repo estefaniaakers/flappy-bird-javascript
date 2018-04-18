735839531d9d3cbf9738171f016097aa8745673c
Estefania Akers <estefaniaakers@hotmail.com>
2018-04-18 14:31:37 -0500

    adding score and sounds

--- flappyBird.js
+++ flappyBird.js
@@ -22,11 +22,21 @@ var constant = pipeNorth.height+gap;
 var bX = 10;
 var bY = 150
 var gravity = 1.5;
+var score = 0;
+
+//audio files
+
+var fly = new Audio();
+var scor = new Audio();
+
+fly.src = "Sounds/fly.mp3"
+scor.src = "Sounds/score.mp3"
 
 //key pressed
 document.addEventListener('keydown',moveUp);
 function moveUp(){
     bY -= 25;
+    fly.play();
 
 }
 
@@ -63,9 +73,15 @@ function draw(){
         if(bX + bird.width >= pipe[i].x && 
            bX <= pipe[i].x + pipeNorth.width &&
            (bY <= pipe[i].y + pipeNorth.height || 
-            bY+bird.height >= pipe[i].y + constant)){
+            bY+bird.height >= pipe[i].y + constant) ||
+            bY+bird.height >= cvs.height - fg.height){
             location.reload(); //reload the page
 
+        }
+
+        if(pipe[i].x == 5){
+            score++;
+            scor.play();
         }
     
     }
@@ -74,6 +90,11 @@ function draw(){
     ctx.drawImage(bird,bX,bY);
 
     bY += gravity;
+
+    ctx.fillStyle = "#000";
+    ctx.font = "20px Verdana";
+    ctx.fillText("Score: "+score,10,cvs.height-20);
+
     requestAnimationFrame(draw);
 }
 
