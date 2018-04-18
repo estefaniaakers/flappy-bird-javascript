e381f79fc6a1529218942c93aa45cdc105200501
Estefania Akers <estefaniaakers@hotmail.com>
2018-04-18 13:50:42 -0500

    adding detect collision, reloading page

--- flappyBird.js
+++ flappyBird.js
@@ -57,6 +57,16 @@ function draw(){
         });
 
         }
+
+    //detect collision
+
+        if(bX + bird.width >= pipe[i].x && 
+           bX <= pipe[i].x + pipeNorth.width &&
+           (bY <= pipe[i].y + pipeNorth.height || 
+            bY+bird.height >= pipe[i].y + constant)){
+            location.reload(); //reload the page
+
+        }
     
     }
     
