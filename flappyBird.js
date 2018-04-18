9026eb1738729a8b8ef71cfc6d1c5c26cf8aca30
Estefania Akers <estefaniaakers@hotmail.com>
2018-04-18 11:37:49 -0500

    first commit

---
+++ flappyBird.js
@@ -0,0 +1,46 @@
+var cvs = document.getElementById("canvas");
+var ctx = cvs.getContext("2d");
+
+// load images
+
+var bird = new Image();
+var bg = new Image();
+var fg = new Image();
+var pipeNorth = new Image();
+var pipeSouth = new Image();
+
+bird.src = "Images/bird.png";
+bg.src = "Images/bg.png";
+fg.src = "images/fg.png";
+pipeNorth.src = "images/pipeNorth.png";
+pipeSouth.src = "images/pipeSouth.png";
+
+// draw images
+
+function draw(){}
+
+draw();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
